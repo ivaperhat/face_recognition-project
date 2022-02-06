affa8b7f779aa7692d4d186e9e458da40df469ee
ivaperhat <59962754+ivaperhat@users.noreply.github.com>
2022-02-06 17:12:14 +0100

    Variable names updated

--- data_access.py
+++ data_access.py
@@ -9,8 +9,8 @@ ACCESS_KEY = 'AKIATUBPYMZCWOIYMNGA'
 SECRET_ACCESS_KEY = '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'
 
 # MySQL Statements
-faces_select_ALL = "SELECT * FROM faces"
-faces_select_IMG = "SELECT img_id FROM faces WHERE face_id = %s"
-faces_delete_ID = "DELETE FROM faces WHERE face_id = %s"
-faces_insert_NAME_ENCODINGS_IMG = "INSERT INTO faces (name, face_encodings, img_id) VALUES (%s, %s, %s)"
-faces_exists_ID = "SELECT EXISTS(SELECT * FROM faces WHERE face_id = %s)"
+FACES_SELECT_ALL = "SELECT * FROM faces"
+FACES_SELECT_IMG = "SELECT img_id FROM faces WHERE face_id = %s"
+FACES_DELETE_RECORD = "DELETE FROM faces WHERE face_id = %s"
+FACES_INSERT = "INSERT INTO faces (name, face_encodings, img_id) VALUES (%s, %s, %s)"
+FACES_EXISTS = "SELECT EXISTS(SELECT * FROM faces WHERE face_id = %s)"
